6e9a35459490df281f9c874dc5701b9ede172c1a
Tommy Lin <tommy.lin@rotman.utoronto.ca>
2022-05-09 12:24:27 -0400

    darkMode done except MUI Comp

--- src/Countries.js
+++ src/Countries.js
@@ -37,42 +37,44 @@ function Countries({ countries }) {
       }
   }
   return (
-    <div className="Countries-container">
-      <div className="Countries-toolbar">
-      <TextField className="Countries-search" onChange={handleSearch}
-      label={
-        <Fragment>
-          <SearchIcon />Search for a country...
-        </Fragment>} />
-      <FormControl className='Countries-select'>
-        <InputLabel>Filter by Region</InputLabel>
-        <Select label="filter-by-region" value={filterVal} onChange={handleFilter}>
-          <MenuItem value="Africa">Africa</MenuItem>
-          <MenuItem value="Americas">Americas</MenuItem>
-          <MenuItem value="Asia">Asia</MenuItem>
-          <MenuItem value="Europe">Europe</MenuItem>
-          <MenuItem value="Oceania">Oceania</MenuItem>
-          <MenuItem value="Antarctic">Antarctic</MenuItem>
-          <MenuItem value="All">All</MenuItem>
-        </Select>
-      </FormControl>
-      </div>
-      <div className="Countries">
-        {showCountries.map(country => (
-          <div className="card"
-          key={country.name.common}
-          style={showCountries.indexOf(country) % 4 !== 3 ? {margin: '2rem 5% 2rem 0'} : {margin: '2rem 0'}}>
-            <img className="card-img-top" src={country.flags[1]} alt="Card image cap" />
-            <div className="card-body">
-              <Link className="Country-link" to={`/country/${country.name.common}`}>
-              <h5 className="card-title">{country.name.common}</h5>
-              </Link>
-              <p><span>Population: </span>{country.population.toLocaleString()}</p>
-              <p><span>Region: </span>{country.region}</p>
-              <p><span>Capital: </span>{country.capital[0] ? country.capital[0] : "N/A"}</p>
+    <div className={isDarkMode ? 'Countries-background-dk' : 'Countries-background'}>
+      <div className="Countries-container">
+        <div className="Countries-toolbar">
+          <TextField className='Countries-search' onChange={handleSearch}
+            label={
+              <Fragment>
+                <SearchIcon />Search for a country...
+              </Fragment>} />
+          <FormControl className='Countries-select'>
+            <InputLabel>Filter by Region</InputLabel>
+            <Select label="filter-by-region" value={filterVal} onChange={handleFilter}>
+              <MenuItem value="Africa">Africa</MenuItem>
+              <MenuItem value="Americas">Americas</MenuItem>
+              <MenuItem value="Asia">Asia</MenuItem>
+              <MenuItem value="Europe">Europe</MenuItem>
+              <MenuItem value="Oceania">Oceania</MenuItem>
+              <MenuItem value="Antarctic">Antarctic</MenuItem>
+              <MenuItem value="All">All</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+        <div className="Countries">
+          {showCountries.map(country => (
+            <div className={isDarkMode ? "card-dk" : "card"}
+            key={country.name.common}
+            style={showCountries.indexOf(country) % 4 !== 3 ? {margin: '2rem 5% 2rem 0'} : {margin: '2rem 0'}}>
+              <img className="card-img-top" src={country.flags[1]} alt="Card image cap" />
+              <div className="card-body">
+                <Link className="Country-link" to={`/country/${country.name.common}`}>
+                <h5 className='card-title'>{country.name.common}</h5>
+                </Link>
+                <p><span>Population: </span>{country.population.toLocaleString()}</p>
+                <p><span>Region: </span>{country.region}</p>
+                <p><span>Capital: </span>{country.capital[0] ? country.capital[0] : "N/A"}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   )
--- src/Navbar.js
+++ src/Navbar.js
@@ -6,11 +6,13 @@ import './Navbar.css'
 function Navbar(props) {
   const { isDarkMode, toggleTheme } = useContext(ThemeContext)
   return (
-    <div className='Navbar'>
-      <h2>Where in the world?</h2>
-      <div className='Navbar-right' onClick={toggleTheme}>
-        <DarkModeIcon style={{marginRight: '0.3rem'}}/>
-        <p>Dark Mode</p>
+    <div className={isDarkMode ? 'Navbar-bg-dk' : "Navbar-bg"}>
+      <div className='Navbar'>
+        <h2>Where in the world?</h2>
+        <div className='Navbar-right' onClick={toggleTheme}>
+          <DarkModeIcon style={{marginRight: '0.3rem'}}/>
+          <p>Dark Mode</p>
+        </div>
       </div>
     </div>
   )
