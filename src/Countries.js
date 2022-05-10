108e42dc8f489a6977fd691cabd75604ebb42e8a
Tommy Lin <tommy.lin@rotman.utoronto.ca>
2022-05-09 22:00:05 -0400

    search and filter complete

--- src/Countries.js
+++ src/Countries.js
@@ -1,4 +1,4 @@
-import { Fragment, useState, useEffect, useContext } from 'react';
+import { Fragment, useState, useContext } from 'react';
 import { Link } from 'react-router-dom'
 import { MenuItem, Select, FormControl, InputLabel, TextField } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search';
@@ -8,49 +8,48 @@ import './Countries.css'
 
 function Countries({ countries }) {
   const { isDarkMode } = useContext(ThemeContext)
-
-  const [showCountries, setShowCountries] = useState(countries)
   const [searchVal, setSearchVal] = useState("")
-  const [filterVal, setFilterVal] = useState("")
+  const [filterVal, setFilterVal] = useState("All")
 
-  useEffect(() => {
-    if(showCountries.length) {
-      setShowCountries(showCountries)
-    } else {
-      setShowCountries(countries)
-    }
-  })
-
-  const handleSearch = (e) => {
+  const handleChange = (e) => {
     setSearchVal(e.target.value)
-    if(e.target.value) {
-      setShowCountries(showCountries.filter(country => country.name.common.toLowerCase().includes(e.target.value.toLowerCase())))        
-    } else {
-      setShowCountries(countries)
-    }
   }
   const handleFilter = (e) => {
     setFilterVal(e.target.value)
-    if(e.target.value !== 'All') {
-      setShowCountries(showCountries.filter(country => country.region === e.target.value))
-    } else if(e.target.value === 'All') {
-        setShowCountries(countries)
-      }
   }
+
+  const search = (items) => {
+    return items.filter(item => {
+      if(item.region === filterVal) {
+        return item['name'].common
+        .toLowerCase()
+        .indexOf(searchVal.toLowerCase()) > -1 
+      } else if(filterVal === 'All') {
+        return item['name'].common
+        .toLowerCase()
+        .indexOf(searchVal.toLowerCase()) > -1 
+      }
+    })
+  } 
+  const filteredCountries = search(countries)
+ 
   return (
     <div className={isDarkMode ? 'Countries-bg-dk' : 'Countries-bg'}>
       <div className="Countries-container">
         <div className="Countries-toolbar">
           <TextField 
+            onChange={handleChange}
             sx={isDarkMode ? {...searchStyleDk} : {...searchStyle}}
-            onChange={handleSearch}
             label={searchVal ? "" : <Fragment><SearchIcon />Search for a country...</Fragment>}
             InputLabelProps={{shrink: false, style: {color: 'gray'}}}/>
           <FormControl className='Countries-select' >
             <InputLabel shrink={false} style={{color: 'gray'}}>
               {filterVal ? "" : "Filter by Region"}
             </InputLabel>
-            <Select value={filterVal} onChange={handleFilter} sx={isDarkMode ? {...selectStyleDk} : {...selectStyle}}>
+            <Select 
+              value={filterVal} 
+              onChange={handleFilter} 
+              sx={isDarkMode ? {...selectStyleDk} : {...selectStyle}}>
               <MenuItem value="Africa">Africa</MenuItem>
               <MenuItem value="Americas">Americas</MenuItem>
               <MenuItem value="Asia">Asia</MenuItem>
@@ -62,10 +61,10 @@ function Countries({ countries }) {
           </FormControl>
         </div>
         <div className="Countries">
-          {showCountries.map(country => (
+          {countries[0] && filteredCountries.map(country => (
             <div className={isDarkMode ? "card-dk" : "card"}
             key={country.name.common}
-            style={showCountries.indexOf(country) % 4 !== 3 ? {margin: '2rem 5% 2rem 0'} : {margin: '2rem 0'}}>
+            style={filteredCountries.indexOf(country) % 4 !== 3 ? {margin: '2rem 5% 2rem 0'} : {margin: '2rem 0'}}>
               <img className="card-img-top" src={country.flags[1]} alt="Card image cap" />
               <div className="card-body">
                 <Link className={isDarkMode ? 'Country-link-dk' : 'Country-link'} to={`/country/${country.name.common}`}>
